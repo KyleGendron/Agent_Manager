110a1b3603ac040d873e34fab527ddcb81edcccb
KyleGendron <kylegendron3@gmail.com>
2017-02-08 10:08:33 -0500

    Finished GUI Menu, hooked up Controller logic to view. Tested buttons.

--- Agent_Manager/src/main/java/com/voice/app/Agent_Manager/controller/MainMenuController.java
+++ Agent_Manager/src/main/java/com/voice/app/Agent_Manager/controller/MainMenuController.java
@@ -1,5 +1,8 @@
 package com.voice.app.Agent_Manager.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import com.voice.app.Agent_Manager.view.MainMenuView;
 
 /**
@@ -7,7 +10,7 @@ import com.voice.app.Agent_Manager.view.MainMenuView;
  * @author Kyle Gendron
  *
  */
-public class MainMenuController {
+public class MainMenuController implements ActionListener{
 	private MainMenuView view;
 	
 	/**
@@ -23,5 +26,38 @@ public class MainMenuController {
 	 */
 	public void initializeGUI(){
 		this.view.initialize();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch(e.getActionCommand()){
+		case "Add Leads From File":
+			System.out.println("Adding Leads from file.");
+			break;
+		case "Add Leads Manually":
+			System.out.println("Adding Leads manually.");
+			break;
+		case "Add Stats From File":
+			System.out.println("Adding Stats from file.");
+			break;
+		case "Add Stats Manually":
+			System.out.println("Adding Stats manually.");
+			break;
+		case "Display All Lead Records":
+			System.out.println("Displaying all Lead Records.");
+			break;
+		case "Display All Stats Records":
+			System.out.println("Displaying all Stats Records.");
+			break;
+		case "Search All Lead Records":
+			System.out.println("Searching all Leads Records.");
+			break;
+		case "Search All Stats Records":
+			System.out.println("Searching all Stats Records.");
+			break;
+		case "Exit":
+			System.exit(0);
+			break;
+		}
 	}
 }
--- Agent_Manager/src/main/java/com/voice/app/Agent_Manager/view/MainMenuView.java
+++ Agent_Manager/src/main/java/com/voice/app/Agent_Manager/view/MainMenuView.java
@@ -35,19 +35,72 @@ public class MainMenuView extends JFrame{
 		menu.setMnemonic(KeyEvent.VK_F);
 		
 		//create menu items top to bottom
-		JMenu addLeadsSubMenu = new JMenu("Add Leads");
-		addLeadsSubMenu.setMnemonic(KeyEvent.VK_A);
 		
-		JMenuItem addLeadsFromFile = new JMenuItem("Add From File");
-		JMenuItem addLeadsIndividually = new JMenuItem("Add Manually");
+		//create add leads submenu
+		JMenu addLeadsSubMenu = new JMenu("Add Leads");
+		addLeadsSubMenu.setMnemonic(KeyEvent.VK_L);
+		
+		//create add leads submenu items and add them
+		JMenuItem addLeadsFromFile = new JMenuItem("Add Leads From File");
+		addLeadsFromFile.addActionListener(controller);
+		JMenuItem addLeadsIndividually = new JMenuItem("Add Leads Manually");
+		addLeadsIndividually.addActionListener(controller);
+		addLeadsSubMenu.add(addLeadsFromFile);
+		addLeadsSubMenu.add(addLeadsIndividually);
+		
+		//create add stats submenu
+		JMenu addStatsSubMenu = new JMenu("Add Stats");
+		addStatsSubMenu.setMnemonic(KeyEvent.VK_R);
+		
+		//create add stats submenu items and add them
+		JMenuItem addStatsFromFile = new JMenuItem("Add Stats From File");
+		addStatsFromFile.addActionListener(controller);
+		JMenuItem addStatsIndividually = new JMenuItem("Add Stats Manually");
+		addStatsIndividually.addActionListener(controller);
+		addStatsSubMenu.add(addStatsFromFile);
+		addStatsSubMenu.add(addStatsIndividually);
 		
 		JMenuItem displayAllLeads = 
 				new JMenuItem("Display All Lead Records");
+		displayAllLeads.addActionListener(controller);
 		JMenuItem displayAllStats = 
 				new JMenuItem("Display All Stats Records");
-		JMenuItem exit = new JMenuItem("Exit");
-		//add all menu items
+		displayAllStats.addActionListener(controller);
 		
+		//create search submenu
+		JMenu searchMenu = new JMenu("Search");
+		searchMenu.setMnemonic(KeyEvent.VK_S);
+		
+		//create search submenu items and add them
+		JMenuItem searchLeads = 
+				new JMenuItem("Search All Lead Records");
+		searchLeads.addActionListener(controller);
+		JMenuItem searchStats = 
+				new JMenuItem("Search All Stats Records");
+		searchStats.addActionListener(controller);
+		searchMenu.add(searchLeads);
+		searchMenu.add(searchStats);
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(controller);
+		
+		//add all menu items
+		menu.add(addLeadsSubMenu);
+		menu.add(addStatsSubMenu);
+		menu.addSeparator();
+		menu.add(displayAllLeads);
+		menu.add(displayAllStats);
+		menu.add(searchMenu);
+		menu.addSeparator();
+		menu.add(exit);
+		
+		//set this menu to the menubar
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		
+		//final frame settings to adjust
+		setTitle("Agent Manager");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
 	}
